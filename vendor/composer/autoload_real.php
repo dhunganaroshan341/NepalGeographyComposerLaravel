07beed73f57e54b28ce1c8751a96ca99a31d1ae4
Roshan Dhungana <dhunganaroshan341@gmail.com>
2026-03-30 12:52:28 +0545

    Fix composer config

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitc9050134575e666b80f27fad66086e07
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitc9050134575e666b80f27fad66086e07', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitc9050134575e666b80f27fad66086e07', 'loadClassLoader'));
